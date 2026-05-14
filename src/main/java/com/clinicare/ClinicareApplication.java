46452c42f1f9c3049b8d69bc926ff2334adf0fb4
a <b>
2026-05-14 16:45:49 +0900

    time slot bug fix

--- src/main/java/com/clinicare/ClinicareApplication.java
+++ src/main/java/com/clinicare/ClinicareApplication.java
@@ -1,5 +1,7 @@
 package com.clinicare;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -8,6 +10,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class ClinicareApplication {
     public static void main(String[] args) {
+        
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Tokyo"));
         SpringApplication.run(ClinicareApplication.class, args);
     }
 }
